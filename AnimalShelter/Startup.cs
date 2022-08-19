a6cac6bf7c274dee2f5198d18f2eca21d6af3ebd
Grace Kostanich <fl5812597@gmail.com>
2022-08-19 15:10:53 -0700

    Clean up code

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -50,7 +50,6 @@ namespace AnimalShelter
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -63,8 +62,6 @@ namespace AnimalShelter
                 });
                 app.UseDeveloperExceptionPage();
             }
-
-            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
