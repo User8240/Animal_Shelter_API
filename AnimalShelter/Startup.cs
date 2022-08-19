c22e1ff61cb56bbf5aa29d23e9a83947d0fd1d41
Grace Kostanich <fl5812597@gmail.com>
2022-08-19 14:13:44 -0700

    WIP: Begin implementing Swagger

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -5,6 +5,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using AnimalShelter.Models;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using System.IO; 
 
 namespace AnimalShelter
 {
@@ -19,18 +23,48 @@ namespace AnimalShelter
 
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddDbContext<AnimalShelterContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Animal Shelter API",
+                    Description = "An ASP.NET Core Web API for managing Animals in an Animal Shelter",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Example Contact",
+                        Url = new Uri("https://example.com/contact")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Example License",
+                        Url = new Uri("https://example.com/license")
+                    }
+                });
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
                 app.UseDeveloperExceptionPage();
             }
+
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
