0bafeee884d4ad9beb1e78365403a7d57b9048a2
Grace Kostanich <fl5812597@gmail.com>
2022-08-19 16:21:05 -0700

    Include Initial migration

---
+++ AnimalShelter/Migrations/20220819224132_Initial.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    AnimalId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(20) CHARACTER SET utf8mb4", maxLength: 20, nullable: false),
+                    Type = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Gender = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.AnimalId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Gender", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, 7, "Female", "Bobby", "Dog" },
+                    { 2, 10, "Female", "Mr. Catto", "Cat" },
+                    { 3, 2, "Female", "Le Doggo", "Dog" },
+                    { 4, 4, "Male", "Gerard", "Cat" },
+                    { 5, 22, "Male", "Noodle", "Dog" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
